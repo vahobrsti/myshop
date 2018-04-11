5835cb7a6f108e25c6c937ab61e25e5c55f9f18f
vahobrsti <vahob.rasti@gmail.com>
2018-04-11 16:38:05 +1000

    test for syncing

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/config.php';
 require_once (ROOTFOLDER.'header.php');
-//for a testing purposes only var_dump($_SESSION);die();
+//this is a test to see var_dump($_SESSION);die();
 ?>
     <a href="/Order/Orders.php?action=view">View your basket</a><br>
 <?php
